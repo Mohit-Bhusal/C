2b5e4e56ba33e8f460ddd7d33a0798a0d6891d9a
Mohit Bhusal <mohit.772419@thc.tu.edu.np>
2021-05-04 16:33:17 +0545

    first commit

---
+++ 2nd_prgm_Data_types.c
@@ -0,0 +1,18 @@
+// Data Types in C 
+// 1. int : 1, 2, 3 
+// 2. float : 1.2 + 1 = 2.2
+// 3. char: "A-Z" ,"a-z", "@#$%^&*"
+// 4. Void : it doesnt return anything 
+#include <stdio.h>
+int main(){
+float first;
+int second; 
+printf("enter first number: ");
+scanf("%f",&first);
+printf("Enter second number: ");
+scanf("%d",&second);
+float sum = first + second;  
+
+printf("The sum is %f",sum);  //we use %f for float and *d for intiger
+    return 0;
+}
