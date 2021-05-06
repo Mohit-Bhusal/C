5971f666d03556fb8e32dc7a0e5138ced05e34c1
Mohit Bhusal <mohit.772419@thc.tu.edu.np>
2021-05-06 15:20:07 +0545

    fabonachhi

---
+++ Fibonacchi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a = 0, b = 1, c, n, i;
+    printf("Enter How many fabonachhi numbers you want"); scanf("%d",&n);
+    for(i=1 ; i<n+1 ; i++){
+        c = a+b;
+        printf("%d ",c);
+        a = b;
+        b = c;
+    }       
+}
