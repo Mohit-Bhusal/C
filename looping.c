fc818ce95d06060c87c4b9614a36bf51b04850c3
Mohit Bhusal <mohit.772419@thc.tu.edu.np>
2021-05-06 13:22:32 +0545

    looping

---
+++ looping.c
@@ -0,0 +1,21 @@
+//for loop
+//syntax:     for(initialize; condition; increment/decrement){
+//              statement;
+//            }
+//while loop 
+//syntax :        while{
+//                    statement;
+//                }
+//do while loop
+//syntax:          do{
+//                 statement;     
+//                    }while(condition);
+
+#include <stdio.h>
+int main(){
+    char name[] = "mohit";
+    int k;
+    for(k=1;k<=20;k++){
+        printf("%s \n" ,name);
+    }
+}
